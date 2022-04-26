6ba4a4b3e9ea93d7079043e8f3085f1c7c351084
Luis Gonzalo <afthedark@hotmail.com>
2022-04-25 20:07:41 -0400

    Actualizando a Rama Main

---
+++ reportes.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+include "include/head.php";
+include "include/menu_superior.php";
+include "include/menu_principal.php";
+?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    
+  </div>
+  <!-- /.content-wrapper -->
+  <?php
+  include "include/footer.php"
+  ?>
+
