5cab4c939d5aa6a575a1ae85bd87e2d587a3ed2f
Luis Gonzalo <afthedark@hotmail.com>
2022-05-02 16:47:21 -0400

    Facturar y Eliminar Mensajes (¿?)

--- include/head.php
+++ include/head.php
@@ -5,31 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Dashboard</title>
 
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
   <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
   <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.css">
-  <!-- overlayScrollbars -->
   <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
   <script type="text/javascript">
     function confirmardelete()
     {
-      var respuesta = confirm("Está seguro de eliminar la venta");
+      var respuesta = confirm("¿Está seguro de eliminar la venta?");
       if(respuesta==true)
       {
         return true;
@@ -43,7 +34,7 @@
     }
     function confirmarfact()
     {
-      var respuesta = confirm("Está seguro de facturar la venta");
+      var respuesta = confirm("¿Está seguro de facturar la venta?");
       if(respuesta==true)
       {
         return true;
