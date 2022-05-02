58e26d7bf33e7603193a8c58d8b4501296c25d42
ramiromendozag <103544156+ramiromendozag@users.noreply.github.com>
2022-05-02 09:03:06 -0400

    sprint2  completada
    Tarea completada

--- registro_de_ventas.php
+++ registro_de_ventas.php
@@ -1,66 +1,87 @@
 <?php 
-    session_start();
-    if($_SESSION['rol']!=1 && $_SESSION['rol']!=3 )
-    {
-        header('location: inicio.php');
-    }
-    include "include/head.php";
-    include "include/menu_superior.php";
-    include "include/menu_principal.php";
+session_start();
+if($_SESSION['rol']!=1 && $_SESSION['rol']!=3 )
+{
+  header('location: inicio.php');
+}
+include "include/head.php";
+include "include/menu_superior.php";
+include "include/menu_principal.php";
 ?>
-<div class="content-wrapper">  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
     <br>
-    <section class="content">    
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6 offset-md-3">
-                    <div class="card">
-                        <div class="card-body register-card-body">
-                            <h3 class="login-box-msg">Formulario de Registro de Ventas</h3>
-                            <form action="pr_regusuario.php" method="GET">
-                                <div class="card-body " >
-                                    <div class="form-group input-group-append">
-                                        <input type="text" class="form-control" name="num_serie" placeholder="Número de Serie" required autocomplete="off">
-                                        <span class="text-danger">  *</span>
-                                    </div>
-                                    <div class="form-group input-group-append">
-                                        <input class="form-control"  type="text" name="fecha_garantia" placeholder="Tiempo de garantía" required autocomplete="off" onclick="ocultarError();" onfocus="(this.type='date')"><span class="text-danger">  *</span> 
-                                    </div>
-                                    <div class="form-group input-group-append">
-                                        <input class="form-control" type="text" name="codigo_producto" placeholder="Código de Producto" required autocomplete="off"><span class="text-danger">  *</span> 
-                                    </div>
+   <!-- Main content -->
+   
+   <section class="content">
+      <div class="container-fluid">
+         <div class="row">
+            <!-- left column -->
+            <div class="col-md-6 offset-md-3">
+               <!-- general form elements -->
+               <div class="card">
+                    <div class="card-body register-card-body">
+                        <h3 class="login-box-msg">Formulario de Registro de Ventas</h3>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="pr_regventa.php"  method="GET">
+                            <div class="card-body " >
+                                <div class="form-group input-group-append">
+                                <input type="text" class="form-control" name="numserie" placeholder="Número de Serie" required autocomplete="off">
+                                 <span class="text-danger">  *</span>
                                 </div>
+                                
+                                 
+                                <div class="form-group input-group-append">
+                                <input class="form-control"  type="text" name="fecha" placeholder="Tiempo de garantía" required autocomplete="off" onclick="ocultarError();" onfocus="(this.type='date')"><span class="text-danger">  *</span> 
+                                </div>
+
+
+                                <div class="form-group input-group-append">
+                                <input class="form-control" type="text" name="codpro" placeholder="Código de Producto" required autocomplete="off"><span class="text-danger">  *</span> 
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                                    
                                 <center>  
+
                                     <?php 
-                                        if(isset($_GET['msj'])){
-                                            $mensaje=$_GET['msj'];
-                                            echo '<b class=msj>'.$mensaje.'</b>';
-                                        }
-                                        if(isset($_GET['msj2'])){
-                                        $mensaje=$_GET['msj2'];
-                                            echo '<b class=msj2>'.$mensaje.'</b>';
-                                        }
-                                    ?>
-                                    <br>
+                                    if(isset($_GET['msj'])){
+                                       $mensaje=$_GET['msj'];
+                                        echo '<b class=msj>'.$mensaje.'</b>';
+                                    }
+                                    if(isset($_GET['msj2'])){
+                                       $mensaje=$_GET['msj2'];
+                                        echo '<b class=msj2>'.$mensaje.'</b>';
+                                    }
+                                     ?>
+                                     <br>
                                     <button type="submit" class="btn btn-primary btn-lg">Registrar</button>
                                 </center>
-                            </form>
-                        </div>
+                            
+                        </form>
                     </div>
                 </div>
+               <!-- /.card -->
             </div>
-        </div>
-    </section>
-    <style type="text/css">
-        .msj{
-            color: red;
-        }
-        .msj2{
-            color: green;
-        }
-    </style>   
+            <!--/.col (left) -->
+         </div>
+         <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+   </section>
+<style type="text/css">
+    .msj{
+        color: red;
+    }
+    .msj2{
+        color: green;
+    }
+</style>   
+   <!-- /.content -->
 </div>
-<?php
-    include "include/footer.php"
-?>
+  <!-- /.content-wrapper -->
+  <?php
+  include "include/footer.php"
+  ?>
 
