758a3e0f140f72e096dba42c94375636d098cdea
Luis Gonzalo <afthedark@hotmail.com>
2022-05-02 17:16:51 -0400

    No se podrán facturar ventas eliminadas

--- facturar_venta.php
+++ facturar_venta.php
@@ -1,15 +1,26 @@
-<?php   
+<?php
 session_start();
 //Facturar Ventas
 include "conexion.php";
 
-$id_venta= $_REQUEST['id'];
-$query_delete=mysqli_query($conection,"UPDATE venta SET estado=2 where  id_venta=$id_venta and id_usuario=".$_SESSION['idUser']."" );
-if($query_delete)
-	{
-		header("location: ventas_registradas.php");
-	}
-	else{
-		echo "Error al eliminar";
-	}
+$id_venta = $_REQUEST["id"];
+$con = "SELECT estado FROM venta where id_venta=$id_venta";
+$query = mysqli_query($conection, $con);
+$result = mysqli_fetch_array($query);
+if ($result[0] == 1) {
+    $query_delete = mysqli_query(
+        $conection,
+        "UPDATE venta SET estado=2 where  id_venta=$id_venta and id_usuario=" .
+            $_SESSION["idUser"] .
+            ""
+    );
+    if ($query_delete) {
+        header("location: ventas_registradas.php");
+    } else {
+        echo "Error al Facturar";
+    }
+} else {
+    header("location: ventas_registradas.php");
+}
+
 ?>
