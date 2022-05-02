223aeaf1c962b61f8372db1aeef0c378c7a656a5
jhonatanR08 <103544233+jhonatanR08@users.noreply.github.com>
2022-05-01 22:40:21 -0400

    mostrar ventas,facturar,eliminar

---
+++ mostrar ventas registradas encargado jhonatan/conexion.php
@@ -0,0 +1,18 @@
+<?php 
+	//conexiones
+	$host = 'localhost';
+	$user = 'root';
+	$password = '';
+	$db = 'dbrma';
+
+	$conection = @mysqli_connect($host,$user,$password,$db);
+
+	if(!$conection){
+		echo "Error en la conexión";
+	}
+	else
+	{
+		
+	}
+
+?>
---
+++ mostrar ventas registradas encargado jhonatan/facturar_venta.php
@@ -0,0 +1,13 @@
+<?php   
+include "conexion.php";
+
+$id_venta= $_REQUEST['id'];
+$query_delete=mysqli_query($conection,"UPDATE venta SET estado=2 where id_venta=$id_venta");
+if($query_delete)
+	{
+		header("location: ventas_registradas.php");
+	}
+	else{
+		echo "Error al eliminar";
+	}
+?>
